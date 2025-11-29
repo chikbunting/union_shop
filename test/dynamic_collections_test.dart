8340fe4335e2d7fefb1088afdc29d5f860c8e49d
chikbunting <chikbunting04@gmail.com>
2025-11-29 23:44:49 +0000

    Add tests for ProductService to verify collections and product retrieval

--- test/dynamic_collections_test.dart
+++ test/dynamic_collections_test.dart
@@ -3,11 +3,12 @@ import 'package:union_shop/services/product_service.dart';
 
 void main() {
   test('ProductService returns collections and products', () {
-    final collections = ProductService.getCollections();
+    final service = ProductService.instance;
+    final collections = service.getCollections();
     expect(collections.isNotEmpty, true);
 
     final first = collections.first;
-    final products = ProductService.getProductsForCollection(first);
+    final products = service.getProductsForCollection(first);
     expect(products, isNotEmpty);
   });
 }
