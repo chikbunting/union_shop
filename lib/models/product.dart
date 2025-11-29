3f68322a7e640658626a24e848ea89cfd2d639e8
chikbunting <chikbunting04@gmail.com>
2025-11-29 22:49:41 +0000

    Add Product model with properties for id, title, price, imageUrl, and collection

---
+++ lib/models/product.dart
@@ -0,0 +1,15 @@
+class Product {
+  final String id;
+  final String title;
+  final double price;
+  final String imageUrl;
+  final String collection;
+
+  const Product({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.imageUrl,
+    required this.collection,
+  });
+}
