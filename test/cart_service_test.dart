cad1607e5a435d894a69cb6e1aa6d34cf62d3d07
chikbunting <chikbunting04@gmail.com>
2025-12-05 09:56:54 +0000

    Remove redundant tests from cart_service_test.dart and streamline test structure

--- test/cart_service_test.dart
+++ test/cart_service_test.dart
@@ -62,26 +62,3 @@ void main() {
     }
   });
 }
-import 'package:flutter_test/flutter_test.dart';
-import 'package:union_shop/services/cart_service.dart';
-import 'package:union_shop/models/product.dart';
-
-void main() {
-  test('cart service add and totals', () {
-    final cart = CartService.instance;
-    cart.clear();
-
-    const p = Product(
-      id: 't1',
-      title: 'Test Product',
-      price: '£2.50',
-      description: 'desc',
-      collection: 'Test',
-      imageUrl: '',
-    );
-
-    cart.add(p, quantity: 2);
-    expect(cart.totalItems, 2);
-    expect(cart.totalPrice, closeTo(5.0, 0.001));
-  });
-}
